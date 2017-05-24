914f79ccb912e7fdb30347b7d0f3f6cae38a3fc8
Reno Syahputra <syahputrareno975@gmail.com>
2017-05-24 21:59:22 +0700

    custom cookie

---
+++ README.md
@@ -0,0 +1,42 @@
+## Golang : custom cookie
+
+paket dibutuhkan : 
+
+	import "github.com/gorilla/securecookie"
+	import "net/http"
+
+penggunaan : 
+
+jadikan 1 direktori dengan program go lainya agar bisa digunakan
+
+program sederhana untuk menetapkan nama cookie dan juga valuenya
+cara penggunaan sangat sederhana, terdapat 3 fungsi dalam program
+ini yaitu
+
+* menetapkan cookie
+
+	set_cookie(name_cookie string,value_cookie string,res http.ResponseWriters)
+contoh : 
+	
+	set_cookie("cookie_1","akses",res)
+
+
+* memanggil cookie
+
+	get_cookie(name_cookie string,req *http.Request) string
+
+contoh : 
+
+	get_cookie("cookie_1",req) string
+
+
+* menghapus cookie
+
+	clear_cookie(name_cookie string,res http.ResponseWriters)
+
+contoh : 
+
+	clear_cookie("cookie_1",res)
+
+
+semoga bermanfaat
---
+++ sesi.go
@@ -0,0 +1,42 @@
+package main
+
+import "github.com/gorilla/securecookie"
+import "net/http"
+
+var cookiehandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
+
+func set_cookie(cookie_name string, cookie_value string, res http.ResponseWriter) {
+	value := map[string]string{
+		"name": cookie_value,
+	}
+	if encoded, err := cookiehandler.Encode(cookie_name, value); err == nil {
+		cookie_ku := &http.Cookie{
+			Name:  cookie_name,
+			Value: encoded,
+			Path:  "/",
+		}
+		http.SetCookie(res, cookie_ku)
+	}
+}
+func get_cookie(cookie_name string, req *http.Request) (name_usernya string) {
+	if cookie_ini, err := req.Cookie(cookie_name); err == nil {
+		nilai_cookie := make(map[string]string)
+		if err = cookiehandler.Decode(cookie_name, cookie_ini.Value, &nilai_cookie); err == nil {
+			name_usernya = nilai_cookie["name"]
+		}
+
+	}
+	return name_usernya
+}
+
+func clear_cookie(cookie_name string, res http.ResponseWriter) {
+	bersihkan_cookie_ku := &http.Cookie{
+		Name:   cookie_name,
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(res, bersihkan_cookie_ku)
+}
